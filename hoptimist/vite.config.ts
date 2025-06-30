85cc984387d0a6eb6cba60b7e9dbf8f8a4a23012
Viktoria Kondrashova <kavidmi@gmail.com>
2025-06-30 18:12:26 +0300

    feat: update vite config

--- hoptimist/vite.config.ts
+++ hoptimist/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+});
