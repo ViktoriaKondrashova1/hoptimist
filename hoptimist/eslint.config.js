5812373766a69d01c34d076bcbbeec04182fba36
Viktoria Kondrashova <kavidmi@gmail.com>
2025-06-30 18:08:41 +0300

    feat: update configs

--- hoptimist/eslint.config.js
+++ hoptimist/eslint.config.js
@@ -1,23 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import antfu from "@antfu/eslint-config";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default antfu(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    react: true,
+    typescript: {
+      tsconfigPath: "./tsconfig.json",
+      overrides: {
+        "ts/no-explicit-any": "error",
+        "ts/consistent-type-assertions": ["error", { assertionStyle: "never" }],
+        "node/prefer-global/process": "off",
+        "prefer-const": "off",
+      },
+    },
+    ignores: ["*.config.ts", "*.d.ts"],
+  },
+  {
+    files: ["**/*.test.ts", "**/*.test.tsx"],
+    rules: {
+      "ts/consistent-type-assertions": "off",
+      "ts/no-unsafe-assignment": "off",
+      "ts/no-unsafe-call": "off",
+      "react-hooks-extra/no-unnecessary-use-prefix": "off",
+      "react-hooks-extra/ no-direct-set-state-in-use-effect": "off",
     },
   },
-])
+  {
+    files: ["vite.config.ts"],
+    rules: {
+      "ts/strict-boolean-expressions": "off",
+    },
+  },
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "off",
+    },
+  }
+);
