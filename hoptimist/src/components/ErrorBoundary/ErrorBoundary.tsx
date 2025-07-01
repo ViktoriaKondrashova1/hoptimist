6f030cd3ae5167a11599cb205b8ec6f57923262a
Viktoria Kondrashova <kavidmi@gmail.com>
2025-07-01 17:25:43 +0300

    feat: add router config

---
+++ hoptimist/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+
+export const ErrorBoundary: FC = () => {
+  return <></>;
+};
---
+++ hoptimist/src/pages/UserProfilePage/UserProfilePage.tsx
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+
+export const UserProfilePage: FC = () => {
+  return <></>;
+};
