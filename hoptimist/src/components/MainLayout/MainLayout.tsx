7e7d4c5da1364c41878d06b456769c12028b4d0d
Viktoria Kondrashova <kavidmi@gmail.com>
2025-07-01 17:39:00 +0300

    feat: add mainlayout

--- hoptimist/src/components/MainLayout/MainLayout.tsx
+++ hoptimist/src/components/MainLayout/MainLayout.tsx
@@ -1,5 +1,14 @@
 import type { FC } from "react";
+import { Outlet } from "react-router-dom";
+import { AppFooter } from "../AppFooter/AppFooter";
+import { AppHeader } from "../AppHeader/AppHeader";
 
 export const MainLayout: FC = () => {
-  return <></>;
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <AppHeader></AppHeader>
+      <Outlet />
+      <AppFooter></AppFooter>
+    </div>
+  );
 };
