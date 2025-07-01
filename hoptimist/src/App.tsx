755190f4c934c7722216638b5cdccc1396a0a637
Viktoria Kondrashova <kavidmi@gmail.com>
2025-07-01 17:38:27 +0300

    feat: update main and app files

--- hoptimist/src/App.tsx
+++ hoptimist/src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./shared/configs/router.tsx";
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
