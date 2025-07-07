16ffcbd4b56c7ccedcdc71fdbe2003bc22cfdc27
Viktoria Kondrashova <kavidmi@gmail.com>
2025-07-07 16:51:40 +0300

    feat: add AppText

--- hoptimist/src/pages/MainPage/components/HeroSection/HeroSection.tsx
+++ hoptimist/src/pages/MainPage/components/HeroSection/HeroSection.tsx
@@ -1,21 +1,23 @@
 import type { BaseComponent } from "@/shared/types/common-types";
 import type { FC } from "react";
-import { Grid, Space } from "antd";
+import { Space, Grid } from "antd";
 import { AppTitle } from "@/shared/components/ui/AppTitle/AppTitle";
 import { APP_NAME } from "@/shared/constants/constants";
+import { AppText } from "@/shared/components/ui/AppText/AppText";
 import "./HeroSection.scss";
 
 interface Props extends BaseComponent {}
 
-// const { useBreakpoint } = Grid;
+const { useBreakpoint } = Grid;
 
 export const HeroSection: FC<Props> = ({ testId = "hero-section" }) => {
-  //   const screens = useBreakpoint()
+  const screens = useBreakpoint()
 
   return (
     <div data-testid={testId} className="hero-section">
       <Space direction="vertical" size="large">
         <AppTitle>{APP_NAME}</AppTitle>;
+        <AppText>Craft Beer Spoken Here</AppText>
       </Space>
     </div>
   );
