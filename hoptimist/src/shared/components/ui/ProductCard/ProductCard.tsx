9fafa991e21420b1b9fc52eaa36010f76f98feae
Viktoria Kondrashova <kavidmi@gmail.com>
2025-07-10 15:06:57 +0300

    feat: add product card

---
+++ hoptimist/src/shared/components/ui/ProductCard/ProductCard.tsx
@@ -0,0 +1,74 @@
+import type { FC } from "react";
+import type { Product } from "@/modules/products/types/product-types";
+import type { BaseComponent } from "@/shared/types/common-types";
+import { Card, Flex } from "antd";
+import Meta from "antd/es/card/Meta";
+import { AppTitle } from "../AppTitle/AppTitle";
+import { ProductCardButton } from "@/shared/components/ui/ProductCardButton/ProductCardButton";
+import { PlusOutlined, HeartOutlined } from "@ant-design/icons";
+import "./ProductCard.scss";
+
+interface Props extends BaseComponent {
+  product: Product;
+}
+
+export const ProductCard: FC<Props> = ({
+  testId = "product-card",
+  product,
+}) => {
+  return (
+    <Card
+      data-testid={testId}
+      hoverable
+      className="product-card"
+      cover={
+        product.images && <img alt={product.title} src={product.images[0]} />
+      }
+    >
+      <Meta
+        title={
+          <AppTitle level={4} className="product-title">
+            {product.title}
+          </AppTitle>
+        }
+        description={
+          <div className="product-description">
+            <div className="brewery">{product.brewery}</div>
+            <div className="category">{product.category}</div>
+            <Flex
+              justify="space-between"
+              align="center"
+              className="price-section"
+            >
+              <div className="price">
+                {product.price.discount !== null &&
+                product.price.discount !== "" ? (
+                  <Flex gap="6px">
+                    <span className="original-price">
+                      {product.price.amount}
+                    </span>
+                    <span className="discount-price">
+                      {product.price.discount}
+                    </span>
+                  </Flex>
+                ) : (
+                  product.price.amount
+                )}
+              </div>
+              <Flex gap="small">
+                <ProductCardButton
+                  tooltip="Add to favorites"
+                  icon={<HeartOutlined />}
+                />
+                <ProductCardButton
+                  tooltip="Add to cart"
+                  icon={<PlusOutlined />}
+                />
+              </Flex>
+            </Flex>
+          </div>
+        }
+      />
+    </Card>
+  );
+};
