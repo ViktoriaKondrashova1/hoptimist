0840e3708a5a7eb63842e6f4cae209465a3a9731
Viktoria Kondrashova <kavidmi@gmail.com>
2025-07-31 17:24:16 +0300

    feat: add login page title and link

--- hoptimist/src/pages/LoginPage/LoginPage.tsx
+++ hoptimist/src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,19 @@
 import type { FC } from "react";
+import {Card} from "antd"
+import {AppTitle} from "@/shared/components/ui/AppTitle/AppTitle"
+import {AppText} from "@/shared/components/ui/AppText/AppText"
+import { Link } from 'react-router-dom'
 
 export const LoginPage: FC = () => {
-  return <>Login</>;
+  return (
+    <Card>
+      <AppTitle level={2} style={{ textAlign: 'center' }}>Login</AppTitle>
+      <div style={{ textAlign: 'center' }}>
+        <AppText>
+          Don't have an account?
+          {' '}
+          <Link to="/register">Register</Link>
+        </AppText>
+      </div>
+    </Card>)
 };
