5fa166dd99f5f4923dfc3a20d084be4a2b19832e
Viktoria Kondrashova <kavidmi@gmail.com>
2025-08-01 15:10:46 +0300

    feat: add register page steps

--- hoptimist/src/pages/RegisterPage/RegisterPage.tsx
+++ hoptimist/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,5 +1,40 @@
-import type { FC } from "react";
+import { type FC, useState } from "react";
+import {AppTitle} from "@/shared/components/ui/AppTitle/AppTitle"
+import {AppButton } from "@/shared/components/ui/AppButton/AppButton"
+import {Steps, message} from "antd"
+import {steps} from "./constructor"
 
 export const RegisterPage: FC = () => {
-  return <>Register</>;
+  const [current, setCurrent] = useState<number>(0);
+
+  const next = () => setCurrent(current + 1);
+
+  const prev = () => setCurrent(current - 1);
+
+  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+  return (
+    <>
+      <AppTitle level={2} style={{ textAlign: 'center', fontWeight: 300 }}>Register</AppTitle>
+      <Steps current={current} items={items} style={{width: "60%", margin: "auto"}}/>
+      <div>{steps[current].content}</div>
+      <div style={{ marginTop: 24 }}>
+        {current < steps.length - 1 && (
+          <AppButton type="primary" onClick={() => next()}>
+            Next
+          </AppButton>
+        )}
+        {current === steps.length - 1 && (
+          <AppButton type="primary" onClick={() => message.success('Processing complete!')}>
+            Done
+          </AppButton>
+        )}
+        {current > 0 && (
+          <AppButton style={{ margin: '0 8px' }} onClick={() => prev()}>
+            Back
+          </AppButton>
+        )}
+      </div>
+    </>
+  )
 };
---
+++ hoptimist/src/pages/RegisterPage/constructor.tsx
@@ -0,0 +1,18 @@
+export const steps = [
+  {
+    title: 'Personal Info',
+    content: 'First-content',
+  },
+  {
+    title: 'Shipping',
+    content: 'Second-content',
+  },
+  {
+    title: 'Billing',
+    content: 'Last-content',
+  },
+  {
+    title: 'Finish',
+    content: 'Last-content',
+  },
+];
